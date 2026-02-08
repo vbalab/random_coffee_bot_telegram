ea0ee1b249fa7a4c29d9b43a5eae014f54896847
vbalab <vv.balabaev@gmail.com>
2026-02-08 20:00:37 +0300

    try

--- src/nespresso/api/TEMP.py
+++ src/nespresso/api/TEMP.py
@@ -4,7 +4,7 @@ from nespresso.api.request import (
     GetNesUserFromMyNES,
 )
 
-sample_nes_ids = list(*range(200))
+sample_nes_ids = [*range(200)]
 
 async def FindSomeNesUsers():
     for nes_id in sample_nes_ids:
